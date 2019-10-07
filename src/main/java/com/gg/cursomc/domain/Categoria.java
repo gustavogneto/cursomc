5080a213e1fef85dc8890601eb180362c2657d04
Gustavo Gomes, Genoma <gustavo.g.neto@gmail.com>
2019-10-07 15:21:02 -0300

    Correção de alguns pontos

--- src/main/java/com/gg/cursomc/domain/Categoria.java
+++ src/main/java/com/gg/cursomc/domain/Categoria.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -30,7 +30,6 @@ public class Categoria implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -47,11 +46,6 @@ public class Categoria implements Serializable {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
 			return false;
 		return true;
 	}
