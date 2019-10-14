450ce16c6581ba6a74365cfbb6726d754b186f39
Gustavo Gomes, Genoma <gustavo.g.neto@gmail.com>
2019-10-14 15:40:40 -0300

    implementando estado e cidade

--- src/main/java/com/gg/cursomc/domain/Estado.java
+++ src/main/java/com/gg/cursomc/domain/Estado.java
@@ -1,20 +1,70 @@
 package com.gg.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Estado implements Serializable{
+public class Estado implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	 private Integer id;
-	 private String nome;
+	private Integer id;
+	private String nome;
+	
+	private List<Cidade> cidades = new ArrayList<Cidade>();
+	
+	public Estado() {
+		super();
+
+	}
+
+	public Estado(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Estado [id=" + id + ", nome=" + nome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
