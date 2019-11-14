481379f3a2554208646ff2b89a8c172351ae067d
gustavogneto <32483792+gustavogneto@users.noreply.github.com>
2019-11-14 00:22:31 -0300

    DTO Categoria

--- src/main/java/com/gg/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gg/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,10 @@ package com.gg.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gg.cursomc.domain.Categoria;
+import com.gg.cursomc.dto.CategoriaDTO;
 import com.gg.cursomc.services.CategoriaService;
 
 @RestControllerAdvice
@@ -53,6 +58,14 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj-> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+
 	}
 	
 	
