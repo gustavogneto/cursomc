88d854f120fc24b057766c1065ccf5a131d39176
gustavogneto <32483792+gustavogneto@users.noreply.github.com>
2019-11-14 11:25:33 -0300

    validação de erros nos campos

---
+++ src/main/java/com/gg/cursomc/resources/exceptions/ValidationError.java
@@ -0,0 +1,24 @@
+package com.gg.cursomc.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, long timeStamp) {
+		super(status, msg, timeStamp);
+
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void add(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
